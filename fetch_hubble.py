c5d0c59d9906ead9dd27815a2c8ec49d3b672bdd
Yulia Savanova <sav.jul@gmail.com>
2019-08-11 04:54:47 +0300

    Refactoring

--- fetch_hubble.py
+++ fetch_hubble.py
@@ -4,7 +4,7 @@ import requests
 
 IMAGES_DIR_NAME = "images"
 HUBBLE_IMAGE_URL = "http://hubblesite.org/api/v3/image/{}"
-HUBBLE_COLLECTION_URL = "http://hubblesite.org/api/v3/images/stsci_gallery"
+HUBBLE_COLLECTION_URL = "http://hubblesite.org/api/v3/images/news"
 
 
 def get_file_extension(url):
@@ -14,6 +14,9 @@ def get_file_extension(url):
 
 def fetch_hubble_images(hubble_image_url, images_dir_name, image_id):
     image_info_response = requests.get(hubble_image_url.format(image_id))
+
+    if not image_info_response.ok:
+        return None
 
     try:
         image_files = image_info_response.json()["image_files"]
@@ -26,39 +29,54 @@ def fetch_hubble_images(hubble_image_url, images_dir_name, image_id):
 
         image_response = requests.get(image_url_for_download, verify=False)
 
-        if image_response.ok:
-            with open(image_path, 'wb') as image_file:
-                image_file.write(image_response.content)
-        else:
-            return print("Can't download image with image_id={} by url {}".format(
-                                                                            image_id,
-                                                                            image_url_for_download))
+        if not image_response.ok:
+            return None
+
+        if image_response.text == "{}":  # {} возвращается при несуществующем id фото
+            return None
+
+        with open(image_path, 'wb') as image_file:
+            return image_file.write(image_response.content)
+
     except KeyError:
-        exit("Photos for image_id={} don't exist".format(image_id))
+        return None
 
 
 def download_from_collection(collection_url):
     collection_images_response = requests.get(collection_url)
 
-    if len(collection_images_response.json()) == 0:
-        exit("Collection {} is empty".format(collection_url))
+    if not collection_images_response.ok:
+        return None
 
-    collection_images = []
-    for image in collection_images_response.json():
-        image_id = image["id"]
-        collection_images.append(image_id)
+    try:
+        collection_images_list = collection_images_response.json()
 
-    return collection_images
+        if len(collection_images_list) == 0:  # получаем [] если в коллекции нет фото или нет такого названия коллекции
+            return None
+
+        collection_images_ids = [image["id"] for image in collection_images_list]
+        return collection_images_ids
+
+    except ValueError:
+        return None
 
 
 def main():
     os.makedirs(IMAGES_DIR_NAME, exist_ok=True)
 
-    collection_images = download_from_collection(HUBBLE_COLLECTION_URL)
+    collection_images_ids = download_from_collection(HUBBLE_COLLECTION_URL)
 
-    for image_id in collection_images:
+    if collection_images_ids is None:
+        exit("Collection {} is empty, doesn't exist or error was occurred while downloading".format(HUBBLE_COLLECTION_URL))
+
+    for image_id in collection_images_ids:
         print("=" * 5 + " Image {} in process...".format(image_id))
-        fetch_hubble_images(HUBBLE_IMAGE_URL, IMAGES_DIR_NAME, image_id)
+
+        if fetch_hubble_images(HUBBLE_IMAGE_URL, IMAGES_DIR_NAME, image_id) is None:
+            print("Can't download image with image_id={}".format(image_id))
+            print("=" * 5 + " Failed!")
+            continue
+
         print("=" * 5 + " Done!")
 
     print("*" * 5 + " All images from the collection {} were processed! ".format(HUBBLE_COLLECTION_URL) + "*" * 5)
--- fetch_spacex.py
+++ fetch_spacex.py
@@ -12,29 +12,38 @@ def fetch_spacex_last_launch(images_dir_name, spacex_last_launch_url):
 
     last_launch_response = requests.get(spacex_last_launch_url)
 
+    if not last_launch_response.ok:
+        return None
+
     try:
         last_launch_images = last_launch_response.json()["links"]["flickr_images"]
 
         if len(last_launch_images) == 0:
-            exit("Launch {} doesn't have images".format(spacex_last_launch_url))
+            return None
 
         for image_number, image_link in enumerate(last_launch_images):
             image_response = requests.get(image_link)
 
-            if image_response.ok:
-                with open(image_path.format(image_number + 1), 'wb') as image_file:
-                    image_file.write(image_response.content)
-            else:
-                print("Can't download image by url {}".format(image_link))
+            if not image_response.ok:
                 continue
 
+            with open(image_path.format(image_number + 1), 'wb') as image_file:
+                image_file.write(image_response.content)
+
+        return True
+
+    except ValueError:
+        return None
+
     except KeyError:
-        exit("Can't download launch images by url {}".format(spacex_last_launch_url))
+        return None
 
 
 def main():
     os.makedirs(IMAGES_DIR_NAME, exist_ok=True)
-    fetch_spacex_last_launch(IMAGES_DIR_NAME, SPACEX_LAST_LAUNCH_URL)
+
+    if fetch_spacex_last_launch(IMAGES_DIR_NAME, SPACEX_LAST_LAUNCH_URL) is None:
+        exit("Last launch {} doesn't have images or error was occurred while downloading".format(SPACEX_LAST_LAUNCH_URL))
 
 
 if __name__ == "__main__":
